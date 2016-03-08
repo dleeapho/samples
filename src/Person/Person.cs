8c703110c9cb24e60e3b9ddfb5d31868364e295d
dleeapho <dleeapho@DDDLEEAPHO045>
2016-03-07 18:54:38 -0800

    Add project files.

---
+++ src/Person/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Personal
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+    }
+}
---
+++ src/PersonByAge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Personal;
+
+namespace PersonByAge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person("John", 21),
+                new Person("Joe", 35),
+                new Person("Jane", 30)
+                
+            };
+
+            var sortedByAge = people.OrderByDescending(x => x.Age);
+            foreach (var item in sortedByAge)
+            {
+                Console.WriteLine("{0} is {1} years old", item.Name, item.Age);
+            }
+
+
+        }
+    }
+}
